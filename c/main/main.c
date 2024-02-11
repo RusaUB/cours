d699a3f7ce7d036daedd01b0850ee380cc730d03
Your Name <you@example.com>
2024-02-11 12:11:41 +0100

    fonctions

--- c/main/main.c
+++ c/main/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
-
+double sq4_x(double x)
+{
+    double res = (x < 0) ? NAN : pow(x, 0.25);
+    return res;
+}
 
 int main()
 {
-    char str[] = {'H', 'e', 'l', 'l', 'o', '\0'};
-    char s[] = "Hello"; 
-    char b[] = "Hel"  "lo";
-    printf("%s", str);
+    printf("%f", sq4_x(12));
     return 0;
 }
